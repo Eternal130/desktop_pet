d63dbb7e5bb5388c4e3eff9dd898dca840cca7a7
Eternal130 <2498668228@qq.com>
2026-07-11 11:20:47 +0800

    build(renderer): vendor FriBidi 1.0.16 with thin CMake wrapper for static linking

---
+++ renderer/third_party/fribidi/lib/fribidi-config.h
@@ -0,0 +1,19 @@
+/* fribidi-config.h - generated for static build (no Meson/Autotools) */
+#ifndef FRIBIDI_CONFIG_H
+#define FRIBIDI_CONFIG_H
+
+#define FRIBIDI "fribidi"
+#define FRIBIDI_NAME "GNU FriBidi"
+#define FRIBIDI_BUGREPORT "https://github.com/fribidi/fribidi/issues/new"
+
+#define FRIBIDI_VERSION "1.0.16"
+#define FRIBIDI_MAJOR_VERSION 1
+#define FRIBIDI_MINOR_VERSION 0
+#define FRIBIDI_MICRO_VERSION 16
+#define FRIBIDI_INTERFACE_VERSION 4
+#define FRIBIDI_INTERFACE_VERSION_STRING "4"
+
+/* The size of a `int', as computed by sizeof. */
+#define FRIBIDI_SIZEOF_INT 4
+
+#endif /* FRIBIDI_CONFIG_H */
